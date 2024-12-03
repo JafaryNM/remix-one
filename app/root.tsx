6d8b04fb7dfc04043ddf0072773a9c0363995999
JafaryNM <jaffarymdegela@gmail.com>
2024-12-03 18:09:08 +0300

    remixStyles

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,9 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
+import styles from './styles/main.css'
 
-import "./tailwind.css";
+//import "./tailwind.css";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -17,9 +18,10 @@ export const links: LinksFunction = () => [
     crossOrigin: "anonymous",
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+    rel:"stylesheet",
+    href:styles
+  }
+ 
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,8 @@
 import type { MetaFunction } from "@remix-run/node";
+import {Link} from '@remix-run/react'
+import type { LinksFunction } from "@remix-run/node";
+import homeStyles from '../styles/home.css'
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,9 +11,27 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+// Adding specific styles on LinkFunction
+
+export const links: LinksFunction = () => [
+  
+  {
+    rel:"stylesheet",
+    href:homeStyles
+  }
+ 
+];
+
 export default function Index() {
   return (
-    <h1>Hello remix</h1>
+    <>
+      <h1>Home index page</h1>
+      {/* This working fine but rise some problem */}
+      {/* <a href="/demo">Go to demo page</a> */}
+      <Link to="/demo">Go to demo page</Link>
+
+    </>
+
   );
 }
 
