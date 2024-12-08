b247b6a7b91dae4472ca6f090cf92dc39e7e7aab
Jafary Mdegela <jaffarymdegela@gmail.com>
2024-12-08 17:24:55 +0300

    fixing

---
+++ app/components/MainNavigation.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from '@remix-run/react';
+
+// Navlink is similar to previous link but it receive some active features
+
+function MainNavigation() {
+  return (
+    <nav id="main-navigation">
+      <ul>
+        <li className="nav-item">
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="/notes">My Notes</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default MainNavigation;
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,11 +1,12 @@
-import NewNote from "~/components/newNote"
+//import NewNote from "~/components/newNote"
 import NewNoteStyles from '~/components/NewNote.css'
 import type { LinksFunction } from "@remix-run/node";
+import NewNote from '~/components/NewNote';
 
 export default function NotesPage(){
   return (
     <main>
-        <NewNote/>
+       <NewNote/>
     </main>
   )
 }
