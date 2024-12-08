2c3edeb2e50a207be2f9960caf64ff98a5adce3c
Jafary Mdegela <jaffarymdegela@gmail.com>
2024-12-08 18:17:35 +0300

    addDataRemix

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,7 +1,9 @@
 //import NewNote from "~/components/newNote"
 import NewNoteStyles from '~/components/NewNote.css'
-import type { LinksFunction } from "@remix-run/node";
+import { redirect, type LinksFunction } from "@remix-run/node";
+import type { ActionFunctionArgs } from '@remix-run/node';
 import NewNote from '~/components/NewNote';
+import {getNotes,storeNotes} from '~/data/notes'
 
 export default function NotesPage(){
   return (
@@ -19,3 +21,21 @@ export const links: LinksFunction = () => [
   }
  
 ];
+
+
+export async function action ({request}:ActionFunctionArgs){
+   const formData = await request.formData();
+  //  const noteData ={
+  //   title : formData.get('title'),
+  //   content: formData.get('content')
+  //  }
+   
+   const noteData = Object.fromEntries(formData);
+   const existingNotes=  await getNotes();
+   noteData.id= new Date().toISOString();
+   const updatedNotes = existingNotes.concat(noteData);
+   await  storeNotes(updatedNotes);
+   return redirect('/notes')
+
+
+}
